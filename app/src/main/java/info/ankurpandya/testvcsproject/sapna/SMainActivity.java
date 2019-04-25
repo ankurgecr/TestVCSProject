31cc403cf585520e5be3eddc23642129db015eb8
TmStore <tmstoretest@gmail.com>
2019-04-25 16:54:19 +0530

    sapna project 2(Button+Toast)

--- app/src/main/java/info/ankurpandya/testvcsproject/sapna/SMainActivity.java
+++ app/src/main/java/info/ankurpandya/testvcsproject/sapna/SMainActivity.java
@@ -2,16 +2,33 @@ package info.ankurpandya.testvcsproject.sapna;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import info.ankurpandya.testvcsproject.R;
 
 public class SMainActivity extends AppCompatActivity {
-    TextView tv_first,tv_last;
+    EditText et_first,et_last;
+    Button btn_Submit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_two);
+        et_first=(EditText)findViewById(R.id.et_first);
+        et_last=(EditText)findViewById(R.id.et_last);
+        btn_Submit=(Button)findViewById(R.id.btn_Submit);
+        btn_Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info=("First Name : "+et_first.getText().toString()+"\nLast Name : "+et_last.getText().toString());
+
+
+                Toast.makeText(SMainActivity.this, info, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
