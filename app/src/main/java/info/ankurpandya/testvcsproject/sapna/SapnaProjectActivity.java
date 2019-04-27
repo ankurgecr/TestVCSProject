4cd2122b2d5910e0b8f21bc580f6f3ce5bbf4239
TmStore <tmstoretest@gmail.com>
2019-04-27 19:14:33 +0530

    sapna project 3(Tic Tac Toe)

--- app/src/main/java/info/ankurpandya/testvcsproject/sapna/SapnaProjectActivity.java
+++ app/src/main/java/info/ankurpandya/testvcsproject/sapna/SapnaProjectActivity.java
@@ -8,16 +8,18 @@ import android.view.View;
 import android.widget.Button;
 
 import info.ankurpandya.testvcsproject.R;
+import info.ankurpandya.testvcsproject.sapna.SapnaGame.GameActivity;
 
 public class SapnaProjectActivity extends AppCompatActivity {
 
-    Button btnPro1,btnPro2;
+    Button btnPro1,btnPro2,btnPro3;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sapna_project_list);
         btnPro1=(Button)findViewById(R.id.btn_pro_1);
         btnPro2=(Button)findViewById(R.id.btn_pro_2);
+        btnPro3=(Button)findViewById(R.id.btn_pro_3);
         btnPro1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +33,14 @@ public class SapnaProjectActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(SapnaProjectActivity.this,SMainActivity.class);
                 startActivity(intent);
+            }
+        });
+        btnPro3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SapnaProjectActivity.this, GameActivity.class);
+                startActivity(intent);
+
             }
         });
     }
